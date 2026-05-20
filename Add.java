d5529386b87e9087ec38d892daf24247bc2ae514
mekalatharun77 <mekalatharunyadav@gmail.com>
2026-05-20 13:04:58 +0530

    sub

--- Add.java
+++ Add.java
@@ -1,8 +1,6 @@
 public class Add {
-    public static void main(String[] args){
-        int a=5;
-        int b=6;
-        System.out.println(a+b);
+     public static void main(String[] args){
+        System.out.println("Hello world");
     }
     
 }
